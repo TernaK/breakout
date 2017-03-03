f3b0cf956323b1b03f83cffec3cb580c1ac229c5
Terna <tkpamber@yahoo.com>
2017-03-03 14:25:27 -0800

    Initial Commit

---
+++ breakout/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  breakout
+//
+//  Created by Terna Kpamber on 3/3/17.
+//  Copyright © 2017 Terna Kpamber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
